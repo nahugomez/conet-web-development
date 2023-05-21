b17dac5f709f72c61245e6329136fb1f0b3e1193
Nahuel Gomez <nahuelgomezs@outlook.com>
2023-05-21 19:40:01 -0300

    Hero Text Modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 </p>
                 <p className="text-4xl font-bold md:text-5xl lg:text-7xl">La pagina web que necesitas.</p>
             </div>
-            <p className="mb-4 text-center text-lg">Somos desarrolladores expertos para potenciar la presencia online.</p>
+            <p className="mb-4 text-center text-lg text-gray-700">Somos desarrolladores expertos para potenciar la presencia online.</p>
             <div className="flex w-full flex-col items-center gap-y-2 md:flex-row md:gap-x-2 md:gap-y-0">
                 <Button className={"w-full py-2"}>Nuestros servicios</Button>
                 <Button className={"flex w-full items-center justify-center py-2"} secondary>
