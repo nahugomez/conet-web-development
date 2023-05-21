12522ed7cc3ee2aec4e2a7e91759da5f093c8b7a
Nahuel Gomez <nahuelgomezs@outlook.com>
2023-05-21 19:48:12 -0300

    Pattern Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Pattern from "./components/Pattern";
 
 function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Pattern classname={"mb-20"} />
             <Pricing />
         </div>
     );
