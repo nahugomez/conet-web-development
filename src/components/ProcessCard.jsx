74d6e4bbbd1c13f833e5c06fae56ca8a9bad59b7
Nahuel Gomez <nahuelgomezs@outlook.com>
2023-05-22 18:25:07 -0300

    ProcessCard Created

---
+++ src/components/ProcessCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProcessCard = ({ image, title, description }) => {
+    const BackgroundStyle = {
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    };
+    return (
+        <div className="mb-8 w-full overflow-hidden rounded-3xl border">
+            <div className="h-[230px]" style={BackgroundStyle}></div>
+            <div className="p-6">
+                <h3 className="text-lg font-medium capitalize text-gray-700">{title}</h3>
+                <p className="text-gray-700">{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ProcessCard;
